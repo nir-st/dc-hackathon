5250f02047b274fcc488d50de9559e113f133839
nir-st <nirstein@post.bgu.ac.il>
2020-12-30 11:15:18 +0200

    client connects on TCP and sends team message

--- client.py
+++ client.py
@@ -1,22 +1,46 @@
 from socket import *
 import scapy
+import struct
+
+
+def startUdpSocket(port):
+    """
+    Create UDP socket and receive message
+    """
+    UDPclientSocket = socket(AF_INET, SOCK_DGRAM)
+    UDPclientSocket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+    UDPclientSocket.bind(('', port))
+    print("Client started, listening for offer requests... ")
+    
+    # recive message from server
+    msg_from_server, serverAddress = UDPclientSocket.recvfrom(28)
+    msg = struct.unpack('Ibh', msg_from_server)
+    if msg[0] != 0xfeedbeef or msg[1] != 0x2:
+        return False
+
+    print('unpacked message: ' + str(msg))
+    port = msg[2]
+    UDPclientSocket.close()
+    return (serverAddress[0], port)
+
+
+def createTcpSocket(server_ip, port, team_name):
+    """
+    create TCP socket
+    """
+    print('trying to create a tcp connection on port ' + str(port))
+    TCPclientSocket = socket(AF_INET, SOCK_STREAM)
+    TCPclientSocket.connect(("localhost", port))
+    TCPclientSocket.send((team_name +  '\n').encode())
 
 
 team_name = 'smelly_cat'
 serverPort = 13117
 
-# create UDP socket
-clientSocket = socket(AF_INET, SOCK_DGRAM)
-print("Client started, listening for offer requests... ")
-
-# recive message from server
-msg_from_server, serverAddress = clientSocket.recvfrom(2048)
-print('Received offer from ' + serverAddress + ', attempting to connect...')
-clientSocket.close()
-
-# # create TCP socket
-# clientSocket = socket(AF_INET, SOCK_STREAM)
-# clientSocket.connect((serverAddress, serverPort))
-# clientSocket.send(team_name, + '\n')
-
-# clientSocket.close()
+while True:
+    serverAns = startUdpSocket(serverPort)
+    if serverAns != False:
+        print(serverAns[0])
+        serverIp = serverAns[0]
+        tcpServerPort = serverAns[1]
+        createTcpSocket(serverIp, tcpServerPort, team_name)
--- server.py
+++ server.py
@@ -1,19 +1,68 @@
 from socket import *
 import scapy
 import time
+import struct
+import threading
+
+
+
+def start_udp_server(port):
+    """
+    create UDP socket for broadcasting
+    """
+    UDPserverSocket = socket(AF_INET, SOCK_DGRAM)
+    UDPserverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    UDPserverSocket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+    UDPserverSocket.settimeout(0.2)
+    print('listening on IP address ' + ipaddress)
+    return UDPserverSocket
+
+
+def start_tcp_server(port):
+    """
+    create TCP socket
+    """
+    TCPServerSocket = socket(AF_INET, SOCK_STREAM)
+    TCPServerSocket.bind(("localhost", port))
+    TCPServerSocket.listen(200)
+    return TCPServerSocket
+
+
+def accept_clients(socket):
+    clients = {}
+    timer = time.time() + 10
+    while time.time() < timer:
+        clientSocket = socket.accept()
+        print('client accepted!')
+        team_name = clientSocket.recv(1024).decode()
+        if team_name and clientSocket:
+            clients[team_name] = clientSocket
+    return clients
+
+
+def broadcast_announcements(socket, udp_port, tcp_port):
+    """
+    send 10 broadcast announcments over UDP every second
+    """
+    print('Starting to broadcast')
+    for i in range(10):
+        print('sending message ' + str(i+1))
+        msg = struct.pack('Ibh', 0xfeedbeef, 0x2, tcp_port)
+        socket.sendto(msg, ('<broadcast>', udp_port))
+        print("message sent")
+        time.sleep(1)
+    socket.close()
 
 
 ipaddress = '127.0.0.1'
-serverPort = 13117
+udpServerPort = 13117
+tcpServerPort = 2099
 
-# create UDP socket
-serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind(('', serverPort))
+udpServerSocket = start_udp_server(udpServerPort)
+tcpServerSocket = start_tcp_server(tcpServerPort)
 
-print('listening on IP address ' + ipaddress)
+t1 = threading.Thread(target=broadcast_announcements, args=(udpServerSocket, udpServerPort, tcpServerPort, ))
+t2 = threading.Thread(target=accept_clients, args=(tcpServerSocket, ))
+t1.start()
+t2.start()
 
-# send 10 broadcast announcments every second
-for i in range(10):
-    msg = '0xfeedbeef'
-    serverSocket.send(msg.encode(), ('<broadcast', serverPort))
-    time.sleep(1)
